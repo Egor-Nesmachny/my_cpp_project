526047b1ec73cce365fc731237c86025cd00e168
Egor-Nesmachny <egor.nesmachny08@gmail.com>
2026-09-22 11:26:35 -0700

    Update message in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, I'm Несмачный Егор from group ИУ5-14/4!" << std::endl;
+    std::cout << "Updated message!" << std::endl;
     return 0;
 }
